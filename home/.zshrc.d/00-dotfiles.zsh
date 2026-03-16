4fc11283d5d4f5a2268593596d1a1863ad80707a
Kaixin <k.liu31@lse.ac.uk>
2026-03-16 15:09:06 +0000

    zsh: silence dotfiles helper during startup

--- home/.zshrc.d/00-dotfiles.zsh
+++ home/.zshrc.d/00-dotfiles.zsh
@@ -1,2 +1,15 @@
-alias dot='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-dot config --local status.showUntrackedFiles no
+typeset -g DOTFILES_GIT_DIR=""
+
+if [[ -d "$HOME/.dotfiles/.git" ]]; then
+  DOTFILES_GIT_DIR="$HOME/.dotfiles/.git"
+elif [[ -f "$HOME/.dotfiles/HEAD" ]]; then
+  DOTFILES_GIT_DIR="$HOME/.dotfiles"
+fi
+
+if [[ -n "$DOTFILES_GIT_DIR" ]]; then
+  dot() {
+    git --git-dir="$DOTFILES_GIT_DIR" --work-tree="$HOME" "$@"
+  }
+
+  dot config --local status.showUntrackedFiles no >/dev/null 2>&1 || true
+fi
